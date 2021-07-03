227457404e5e7695fdcdb4f5c30efaf2bcf58c58
zakaria <zakariasamy10@gmail.com>
2021-07-03 19:45:02 +0200

    edit category index page by adding related product and product count for every cat

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -54,6 +54,8 @@
                             <tr>
                                 <th>#</th>
                                 <th>@lang('site.name')</th>
+                                <th>@lang('site.products_count')</th>
+                                <th>@lang('site.related_products')</th>
                                 <th>@lang('site.action')</th>
                             </tr>
                             </thead>
@@ -63,6 +65,8 @@
                                 <tr>
                                     <td>{{ $index + 1 }}</td>
                                     <td>{{ $category->name }}</td>
+                                    <td>{{ $category->products->count() }}</td>
+                                    <td><a href="{{ route('dashboard.products.index', ['category_id' => $category->id]) }}" class="btn btn-info btn-sm">@lang('site.related_products')</a></td>
                                     <td>
                                         @if (auth()->user()->hasPermission('categories-update'))
                                             <a href="{{ route('dashboard.categories.edit', $category->id) }}" class="btn btn-info btn-sm"><i class="fa fa-edit"></i> @lang('site.edit')</a>
